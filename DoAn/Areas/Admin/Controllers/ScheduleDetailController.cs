40e68e5da95acb432a8e7e4bbe3dc5b8adcf3321
hiephuynh <hiephuynh@HIEPHUYNHBF54>
2023-09-08 13:13:53 +0700

    sss

--- DoAn/Areas/Admin/Controllers/ScheduleDetailController.cs
+++ DoAn/Areas/Admin/Controllers/ScheduleDetailController.cs
@@ -1,7 +1,9 @@
 ﻿using DoAn.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace DoAn.Areas.Admin.Controllers
 {
@@ -16,23 +18,103 @@ namespace DoAn.Areas.Admin.Controllers
             _httpClient = new HttpClient();
         }
 
-        //View List
+        //public async Task<IActionResult> Index()
+        //{
+        //    // kiểm tra session 
+        //    if (HttpContext.Session.TryGetValue("UserId", out byte[] userIdBytes))
+        //    {
+        //        if (int.TryParse(Encoding.UTF8.GetString(userIdBytes), out int staffId))
+        //        {
+        //            var apiResponse = await _httpClient.GetAsync("https://localhost:7109/api/ScheduleDetailApi/");
+        //            if (apiResponse.IsSuccessStatusCode)
+        //            {
+        //                var responseContent = await apiResponse.Content.ReadAsStringAsync();
+        //                var schedules = JsonConvert.DeserializeObject<List<Scheduledetail>>(responseContent);
+
+        //                //lọc lịch làm việc dựa theo StaffId
+        //                var filteredSchedules = schedules.Where(s => s.Staff.StaffId == staffId).ToList();
+
+        //                return View(filteredSchedules);
+        //            }
+        //            else
+        //            {
+        //                var schedules = await db.Scheduledetails
+        //                    .Where(s => s.Staff.StaffId == staffId)
+        //                    .ToListAsync();
+
+        //                return View(schedules);
+        //            }
+        //        }
+        //    }
+
+        //    //nếu nhân viên không có lịch làm việc thì thôi không hiện
+        //    return View(new List<Scheduledetail>());
+        //}
+        //public async Task<IActionResult> Index()
+        //{
+        //    if (HttpContext.Session.TryGetValue("UserId", out byte[] userIdBytes))
+        //    {
+        //        if (int.TryParse(Encoding.UTF8.GetString(userIdBytes), out int staffId))
+        //        {
+        //            var apiResponse = await _httpClient.GetAsync("https://localhost:7109/api/ScheduleDetailApi/");
+        //            if (apiResponse.IsSuccessStatusCode)
+        //            {
+        //                var responseContent = await apiResponse.Content.ReadAsStringAsync();
+        //                var schedules = JsonConvert.DeserializeObject<List<Scheduledetail>>(responseContent);
+
+        //                var filteredSchedules = schedules.Where(s => s.Staff.StaffId == staffId).ToList();
+
+        //                return View(filteredSchedules);
+        //            }
+        //            else
+        //            {
+        //                var schedules = await db.Scheduledetails
+        //                    .Where(s => s.Staff.StaffId == staffId)
+        //                    .ToListAsync();
+
+        //                return View(schedules);
+        //            }
+        //        }
+        //    }
+
+        //    ViewBag.LoginMessage = "Please log in to view the schedule.";
+        //    return View(); 
+        //}
         public async Task<IActionResult> Index()
         {
+            if (HttpContext.Session.TryGetValue("UserId", out byte[] userIdBytes))
+            {
+                if (int.TryParse(Encoding.UTF8.GetString(userIdBytes), out int staffId))
+                {
+                    var apiResponse = await _httpClient.GetAsync("https://localhost:7109/api/ScheduleDetailApi/");
+                    if (apiResponse.IsSuccessStatusCode)
+                    {
+                        var responseContent = await apiResponse.Content.ReadAsStringAsync();
+                        var schedules = JsonConvert.DeserializeObject<List<Scheduledetail>>(responseContent);
 
-            var apiResponse = await _httpClient.GetAsync("https://localhost:7109/api/ScheduleDetailApi/");
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                var responseContent = await apiResponse.Content.ReadAsStringAsync();
-                var schedules = JsonConvert.DeserializeObject<List<Scheduledetail>>(responseContent);
-                return View(schedules);
+                        // Check UserRole nếu role = 1 là admin thì được quản lí 
+                        var staff = await db.Staff.FirstOrDefaultAsync(s => s.StaffId == staffId);
+                        if (staff != null)
+                        {
+                            if (staff.RoleId == 1)
+                            {
+                                // Display all schedules for UserRole 1
+                                return View(schedules);
+                            }
+                            else
+                            {
+                                //lọc lịch làm việc dựa theo StaffId
+                                var filteredSchedules = schedules.Where(s => s.Staff.StaffId == staffId).ToList();
+                                return View(filteredSchedules);
+                            }
+                        }
+                    }
+                }
             }
-            else
-            {
-                var schedules = await db.Scheduledetails
-                 .ToListAsync();
-                return View(schedules);
-            }
+
+            ViewBag.LoginMessage = "Please log in to view the schedule.";
+            return View();
         }
+
     }
 }
