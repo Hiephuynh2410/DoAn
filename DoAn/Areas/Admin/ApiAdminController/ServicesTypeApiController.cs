459a82d18b87210fe2ef5ddb67603bdd2475770b
hiephuynh <hiephuynh@HIEPHUYNHBF54>
2023-09-15 19:30:03 +0700

    update admin

---
+++ DoAn/Areas/Admin/ApiAdminController/ServicesTypeApiController.cs
@@ -0,0 +1,135 @@
+﻿using DoAn.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoAn.Areas.Admin.ApiAdminController
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServicesTypeApiController : Controller
+    {
+        private readonly DlctContext _dbContext;
+        public ServicesTypeApiController(DlctContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllServicesType()
+        {
+            var ServicesTypes = await _dbContext.Servicetypes
+                .ToListAsync();
+
+            var ServicesTypesWithFullInfo = ServicesTypes.Select(s => new
+            {
+                s.ServiceTypeId,
+                s.Name,
+               
+            }).ToList();
+
+            return Ok(ServicesTypesWithFullInfo);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateServicesType(Servicetype createModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var ServicetypeExists = await _dbContext.Servicetypes.AnyAsync(b => b.Name == createModel.Name);
+                if (ServicetypeExists)
+                {
+                    return BadRequest(new { Message = "Servicetype already exists." });
+                }
+
+                var newServicetype = new Servicetype
+                {
+                    Name = createModel.Name,
+                };
+
+                _dbContext.Servicetypes.Add(newServicetype);
+                await _dbContext.SaveChangesAsync();
+
+                var registrationSuccessResponse = new
+                {
+                    Message = "Servicetype registration successful",
+                    ServiceTypeId = newServicetype.ServiceTypeId
+                };
+                return Ok(registrationSuccessResponse);
+            }
+
+            var invalidDataErrorResponse = new
+            {
+                Message = "Invalid Servicetype data",
+                Errors = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList()
+            };
+            return BadRequest(invalidDataErrorResponse);
+        }
+
+        [HttpPut("update/{Servicetypeid}")]
+        public async Task<IActionResult> UpdateServicesType(int Servicetypeid, Combo updateModel)
+        {
+            var Servicetype = await _dbContext.Servicetypes.FindAsync(Servicetypeid);
+            if (Servicetype == null)
+            {
+                return NotFound();
+            }
+
+            if (!string.IsNullOrWhiteSpace(updateModel.Name))
+            {
+                Servicetype.Name = updateModel.Name;
+            }
+            
+            _dbContext.Entry(Servicetype).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+
+            var updateSuccessResponse = new
+            {
+                Message = "Servicetype updated successfully"
+            };
+
+            return Ok(updateSuccessResponse);
+        }
+
+        [HttpDelete("delete/{Servicetypeid}")]
+        public async Task<IActionResult> DeleteServicesType(int Servicetypeid)
+        {
+            var Servicetype = await _dbContext.Servicetypes.FindAsync(Servicetypeid);
+            if (Servicetype == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Servicetypes.Remove(Servicetype);
+            await _dbContext.SaveChangesAsync();
+
+            var deleteSuccessResponse = new
+            {
+                Message = "Servicetype deleted successfully"
+            };
+
+            return Ok(deleteSuccessResponse);
+        }
+
+        [HttpGet("detail/{Servicetypeid}")]
+        public async Task<IActionResult> GetServicetypeDetail(int Servicetypeid)
+        {
+            var Servicetype = await _dbContext.Servicetypes.FindAsync(Servicetypeid);
+
+            if (Servicetype == null)
+            {
+                return NotFound();
+            }
+            var ServicetypeDetail = new
+            {
+                Servicetype.ServiceTypeId,
+                Servicetype.Name
+             
+            };
+            return Json(ServicetypeDetail);
+        }
+
+    }
+}
---
+++ DoAn/Areas/Admin/Controllers/ServiceTypeController.cs
@@ -0,0 +1,175 @@
+﻿using DoAn.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceTypeController : Controller
+    {
+        DlctContext db = new DlctContext();
+        private readonly HttpClient _httpClient;
+
+        public ServiceTypeController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        //Delete
+        public async Task<IActionResult> Delete(int Servicetypeid)
+        {
+            if (HttpContext.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Staff");
+            }
+            var apiUrl = $"https://localhost:7109/api/ServicesTypeApi/delete/{Servicetypeid}";
+
+            var response = await _httpClient.DeleteAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("API Response Content: " + responseContent);
+
+                var errorResponse = JsonConvert.DeserializeObject<object>(responseContent);
+
+                ModelState.AddModelError("", errorResponse.ToString());
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        //detail
+        [HttpGet]
+        public async Task<IActionResult> Detail(int Servicetypeid)
+        {
+            if (HttpContext.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Staff");
+            }
+            var apiUrl = $"https://localhost:7109/api/ServicesTypeApi/detail/{Servicetypeid}";
+
+            var apiResponse = await _httpClient.GetAsync(apiUrl);
+            if (apiResponse.IsSuccessStatusCode)
+            {
+                var responseContent = await apiResponse.Content.ReadAsStringAsync();
+                var ServicetypeidDetail = JsonConvert.DeserializeObject<Servicetype>(responseContent);
+
+                return View(ServicetypeidDetail);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        //edit
+        [HttpGet]
+        public IActionResult Edit(int Servicetypeid)
+        {
+            if (HttpContext.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Staff");
+            }
+
+            var Servicetype = db.Servicetypes.Find(Servicetypeid);
+            if (Servicetype == null)
+            {
+                return NotFound();
+            }
+            return View(Servicetype);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int Servicetypeid, Servicetype updateModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(updateModel);
+            }
+           
+            var apiUrl = $"https://localhost:7109/api/ServicesTypeApi/update/{Servicetypeid}";
+
+            var json = JsonConvert.SerializeObject(updateModel);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync(apiUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("API Response Content: " + responseContent);
+
+                var errorResponse = JsonConvert.DeserializeObject<object>(responseContent);
+
+                ModelState.AddModelError("", errorResponse.ToString());
+                return View(updateModel);
+            }
+        }
+
+        //Create
+        public IActionResult Create()
+        {
+            if (HttpContext.Session.GetString("UserId") == null)
+            {
+                return RedirectToAction("Login", "Staff");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Servicetype registrationModel)
+        {
+            var apiUrl = "https://localhost:7109/api/ServicesTypeApi/create";
+            var json = JsonConvert.SerializeObject(registrationModel);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(apiUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("API Response Content: " + responseContent);
+
+
+                var errorResponse = JsonConvert.DeserializeObject<object>(responseContent);
+
+                ModelState.AddModelError("", errorResponse.ToString());
+                return View(registrationModel);
+            }
+        }
+
+        //View List
+        public async Task<IActionResult> Index()
+        {
+
+            var apiResponse = await _httpClient.GetAsync("https://localhost:7109/api/ServicesTypeApi/");
+            if (apiResponse.IsSuccessStatusCode)
+            {
+                var responseContent = await apiResponse.Content.ReadAsStringAsync();
+                var ServicesType = JsonConvert.DeserializeObject<List<Servicetype>>(responseContent);
+
+                return View(ServicesType);
+            }
+            else
+            {
+                var ServicesType = await db.Servicetypes
+                   .ToListAsync();
+                return View(ServicesType);
+            }
+        }
+    }
+}
