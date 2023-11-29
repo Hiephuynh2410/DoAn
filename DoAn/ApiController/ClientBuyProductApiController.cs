3c450ef3829e2af73050f6fd4ca9d3e06b997f45
hiephuynh <hiephuynh@HIEPHUYNHBF54>
2023-11-29 12:45:50 +0700

    updateCart

--- DoAn/ApiController/ClientBuyProductApiController.cs
+++ DoAn/ApiController/ClientBuyProductApiController.cs
@@ -273,49 +273,44 @@ namespace DoAn.ApiController
             }
         }
 
+        [HttpPut("UpdateCart/{userId}/{productId}/{quantity}")]
+        public async Task<IActionResult> UpdateCart(int userId, int productId, int quantity)
+        {
+            try
+            {
+                if (userId <= 0 || productId <= 0 || quantity <= 0)
+                {
+                    return BadRequest("Invalid request parameters.");
+                }
+
+                var existingCartItem = await _dbContext.Carts
+                    .Include(c => c.Product)
+                    .Include(c => c.User)
+                    .Where(c => c.UserId == userId && c.ProductId == productId)
+                    .FirstOrDefaultAsync();
+
+                if (existingCartItem == null)
+                {
+                    return NotFound("CartItem not found.");
+                }
+
+                if (quantity > existingCartItem.Product.Quantity)
+                {
+                    return BadRequest("khong con sp");
+                }
+
+                existingCartItem.Quantity += quantity;
+
+                var updatedTotalAmount = existingCartItem.TotalAmount;
+                await _dbContext.SaveChangesAsync();
+
+                return Ok($"Cart updated successfully. Updated Total Amount: {updatedTotalAmount}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }
-//[HttpPut("UpdateCart/{userId}/{productId}/{quantity}")]
-//public async Task<IActionResult> UpdateCart(int userId, int productId, int quantity)
-//{
-//    try
-//    {
-//        if (userId <= 0 || productId <= 0 || quantity <= 0)
-//        {
-//            return BadRequest("Invalid request parameters.");
-//        }
 
-//        var existingCartItem = await _dbContext.Carts
-//            .Include(c => c.Product)
-//            .Include(c => c.User)
-//            .Where(c => c.UserId == userId && c.ProductId == productId)
-//            .FirstOrDefaultAsync();
-
-//        if (existingCartItem == null)
-//        {
-//            return NotFound("CartItem not found.");
-//        }
-
-//        if (quantity > existingCartItem.Product.Quantity)
-//        {
-//            return BadRequest("Requested quantity exceeds available stock.");
-//        }
-
-//        var originalQuantity = existingCartItem.Quantity;
-
-//        existingCartItem.Product.Quantity -= quantity;
-
-//        existingCartItem.Quantity = quantity;
-
-
-//        var updatedTotalAmount = existingCartItem.TotalAmount;
-//        await _dbContext.SaveChangesAsync();
-
-//        return Ok($"Cart updated successfully. Updated Total Amount: {updatedTotalAmount}");
-
-//    }
-//    catch (Exception ex)
-//    {
-//        return StatusCode(500, $"Internal server error: {ex.Message}");
-//    }
-//}
