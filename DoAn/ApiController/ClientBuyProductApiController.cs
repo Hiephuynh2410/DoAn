7b33edb9932284a12858241d6c7f113e544c5263
hiephuynh <hiephuynh@HIEPHUYNHBF54>
2023-11-29 14:12:48 +0700

    mail when buy

--- DoAn/ApiController/ClientBuyProductApiController.cs
+++ DoAn/ApiController/ClientBuyProductApiController.cs
@@ -1,10 +1,8 @@
 ﻿using DoAn.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Drawing;
-using System.Linq;
-using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace DoAn.ApiController
 {
@@ -192,6 +190,8 @@ namespace DoAn.ApiController
                 _dbContext.Billdetails.Add(newBillDetail);
                 await _dbContext.SaveChangesAsync();
 
+                await SendBookingNotificationEmail(client.Email, newBillDetail, product, client);
+
                 var tongtien = quantityToBuy * product.Price;
                 var responseMessage = $"Product bought successfully. Bill Detail ID: {newBillDetail.BillId}. Total Cost: {tongtien}.";
 
@@ -202,6 +202,87 @@ namespace DoAn.ApiController
                 Console.WriteLine($"Exception: {ex.Message}");
                 Console.WriteLine($"Inner Exception: {ex.InnerException?.Message}");
                 return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        private async Task SendBookingNotificationEmail(string clientEmail, Billdetail billDetail, Product product, Client client)
+        {
+            try
+            {
+                var message = new MimeMessage();
+
+                message.From.Add(new MailboxAddress("New Booking", "huynhhiepvan1998@gmail.com"));
+                message.Subject = "Purchase Confirmation";
+
+                message.Body = new TextPart("html")
+                {
+                    Text = $@"
+                <!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.0 Transitional//EN"" ""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"">
+                <html xmlns=""http://www.w3.org/1999/xhtml"">
+                <head>
+                    <meta http-equiv=""Content-Type"" content=""text/html; charset=utf-8"" />
+                    <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"" />
+                    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+                    <title>Purchase Confirmation</title>
+                </head>
+                <body style=""margin: 0;"">
+                    <center style=""width: 100%; table-layout: fixed; background-color: #e5dec7;"" class=""wrapper"">
+                        <table class=""main"" style=""border-spacing: 0; width: 100%; max-width: 500px; background-color: #ffffff; font-family: sans-serif; color: #4a4a4a; box-shadow: 0 0 25px rgba(0, 0, 0, .15);"" width=""100%"">
+                            <tr>
+                                <td style=""padding: 15px; background-color: #ffffff;"">
+                                    <h2 style=""font-family: 'Roboto Condensed', sans-serif; font-size: 24px; text-align: center; color: #c89800; text-transform: uppercase;"">Xác nhận thanh toán</h2>
+                                    <p style=""font-family: 'Roboto Condensed', sans-serif; font-size: 16px; color: #4a4a4a;"">
+                                        Thân Mến {client.Name},
+                                    </p>
+                                    <p style=""font-family: 'Roboto Condensed', sans-serif; font-size: 16px; color: #4a4a4a;"">
+                                        Cảm ơn bạn đã mua hàng của chúng tôi! Dưới đây là thông tin sản phảm bạn đã mua:
+                                    </p>
+                                    <table style=""border-spacing: 0; font-size: 16px; width: 100%;"">
+                                        <tr>
+                                            <td style=""padding: 10px;""><strong>Product:</strong></td>
+                                            <td style=""padding: 10px;"">{product.Name}</td>
+                                        </tr>
+                                        <tr>
+                                            <td style=""padding: 10px;""><strong>Quantity:</strong></td>
+                                            <td style=""padding: 10px;"">{billDetail.Quantity}</td>
+                                        </tr>
+                                        <tr>
+                                            <td style=""padding: 10px;""><strong>Total Price:</strong></td>
+                                            <td style=""padding: 10px;"">{billDetail.Price}</td>
+                                        </tr>
+                                    </table>
+                                    <p style=""font-family: 'Roboto Condensed', sans-serif; font-size: 16px; color: #4a4a4a;"">
+                                        Cảm ơn bạn đã chọn dịch vụ của chúng tôi. Nếu bạn có câu hỏi nào, hãy cứ liên hệ với chúng tôi khi bạn cảm thấy thoải mái.
+                                    </p>
+                                    <p style=""font-family: 'Roboto Condensed', sans-serif; font-size: 16px; color: #4a4a4a;"">
+                                        Trân trọng kính chào!
+                                        <br />
+                                        Booking
+                                    </p>
+                                </td>
+                            </tr>
+                        </table>
+                    </center>
+                </body>
+                </html>
+            "
+                };
+
+                using (var smtpClient = new SmtpClient())
+                {
+                    smtpClient.Connect("smtp.gmail.com", 587, false);
+                    smtpClient.Authenticate("huynhhiepvan1998@gmail.com", "nmqt ljyf skbz xcrs");
+
+                    message.To.Add(new MailboxAddress(clientEmail, clientEmail));
+
+                    await smtpClient.SendAsync(message);
+
+                    smtpClient.Disconnect(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error sending email: " + ex.Message);
             }
         }
 
